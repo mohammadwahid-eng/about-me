5c95bee30178a705e113a7a375642fa85e28879f
mohammadwahid-eng <mohammadwahid.eng@gmail.com>
2024-04-09 17:32:47 +0600

    footer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ['./index.html'],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
+      },
       fontFamily: {
-        playfair: ["Playfair Display", "serif"],
-        quicksand: ["Quicksand", "sans-serif"],
+        playfair: ['Playfair Display', 'serif'],
+        quicksand: ['Quicksand', 'sans-serif'],
       }
     },
   },
